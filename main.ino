1763cc2d24988cad4d02a060b43cc4d8ff925e7a
Eddyyy <Edward.Thomson9@gmail.com>
2020-03-06 16:15:32 +1100

    Started structure

--- main.ino
+++ main.ino
@@ -42,14 +42,60 @@ void buttonStateCapture()
 {
   if (butPress == 1){
     butPress = 0;
-    loop;
   }
 }
+// System Modes
+#define SYS_MODE_NONE           (0x0000)
+#define SYS_MODE_SET_TEST       (0x0001)
+#define SYS_MODE_RUN_TEST       (0x0002)
+int sys_mode = SYS_MODE_NONE;
+int prev_sys_mode = SYS_MODE_NONE;
+int next_sys_mode = SYS_MODE_NONE;
+
+#define TEST_UNDEFINED          (0x0000)
+#define TEST_MANUAL             (0x0001)
+#define TEST_RAMP               (0x0002)
+#define TEST_HOLD               (0x0003)
+int test_type = TEST_UNDEFINED;
+
 // Main operational loop
-void loop()                     
-{
-  if (digitalRead(butPort) == HIGH && debounce = 256) {
-    butPress = 1;
-  }
-  delay(250);
+void loop() {
+    switch (sys_mode) {
+        case SYS_MODE_NONE:
+            // Reset to defaults
+            Serial.print("---Setting defaults---\n");
+            next_sys_mode = SYS_MODE_SET_TEST;
+            break;
+        case SYS_MODE_SET_TEST:
+            if (prev_sys_mode != sys_mode) {
+                Serial.print("---Test Setup---\n");
+                Serial.print("Manual, Ramp, Hold (M, R, H):\t");
+            }
+
+            if (test_type == TEST_UNDEFINED && Serial.available() > 0) {
+                String usr_ans = Serial.read();
+            }
+            break;
+        case SYS_MODE_RUN_TEST:
+            if (prev_sys_mode != sys_mode) {
+                Serial.print("---Running Test---\n");
+            }
+
+            break;
+        default:
+            next_sys_mode = SYS_MODE_NONE;
+            break;
+    }
+
+    // On state change
+    if (sys_mode != next_sys_mode) {
+        prev_sys_mode = sys_mode;
+        sys_mode = next_sys_mode;
+    }
+    
+
+//  if (digitalRead(butPort) == HIGH && debounce = 256) {
+//    butPress = 1;
+//  }
+//  delay(250);
 }
